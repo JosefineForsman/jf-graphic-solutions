f94d1b500da7790068544e89d78687e750b41536
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-10 12:22:56 +0100

    design fix

--- jf-graphic-solutions/src/views/Home/Home.tsx
+++ jf-graphic-solutions/src/views/Home/Home.tsx
@@ -8,16 +8,20 @@ function Home(){
           </header>
     
           <main>
-            <section>
-              <h1>Våra Tjänster</h1>
-              <p>Här är en lista över tjänster som vi erbjuder:</p>
-              <ul>
-                <li>Drönarfotografering</li>
-                <li>Bildredigering</li>
-                <li>Retuschering</li>
-                <li>Grafisk design</li>
-                <li>Fotografering</li>
-                <li>Videoproduktion</li>
+            <section className='home'>
+              <h1 className='text__title'>Welcome to JF Graphic Solutions</h1>
+              <h2 className='text'>  Maybe you are considering taking the step into social media with effective ads?</h2>
+              <h2 className='text'> JF Grafik Solutions is your trusted partner in photography and video production with customized editing for all your business needs.</h2>
+            </section>
+            <section className='home__services'>
+              <h1 className='text__title'>Our services</h1>
+              <ul className='home__services__ul'>
+                <li className='home__services__options'>Drone Photography</li>
+                <li className='home__services__options'>Image editing</li>
+                <li className='home__services__options'>Retouching</li>
+                <li className='home__services__options'>Graphic Design</li>
+                <li className='home__services__options'>Photographing</li>
+                <li className='home__services__options'>Video Production</li>
               </ul>
             </section>
           </main>
