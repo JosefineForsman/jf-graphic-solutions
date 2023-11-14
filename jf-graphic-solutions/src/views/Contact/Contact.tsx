22d652df39eb41081fb7f773fa7ec61affdc3f64
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-14 13:14:11 +0100

    added css to links

--- jf-graphic-solutions/src/views/Contact/Contact.tsx
+++ jf-graphic-solutions/src/views/Contact/Contact.tsx
@@ -24,19 +24,32 @@ function Contact() {
           <h2 className="contact__title">Follow us on social media</h2>
           <aside className="contact__social-media">
             <img src={facebook} alt="Facebook icon" className="social-media-icon" />
-            <a href="https://www.facebook.com/profile.php?id=100066812072840" target="_blank" rel="noopener noreferrer" aria-label="Visit Facebook page" className="contact__text">
-            Go to our facebook page
+            <a href="https://www.facebook.com/profile.php?id=100066812072840" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              aria-label="Visit Facebook page" 
+              className="contact__text" 
+              title='This is a link to JF graphics facebook page'>
+            Go to our Facebook page
             </a>
           </aside>
           <aside className="contact__social-media">
             <img src={instagram} alt="Instagram icon" className="social-media-icon" />
-            <a href="https://www.instagram.com/jfgraphicsolutions" target="_blank" rel="noopener noreferrer" aria-label="Visit Instagram profile" className="contact__text">
-              Go to our instagram page
+            <a href="https://www.instagram.com/jfgraphicsolutions" 
+              target="_blank" rel="noopener noreferrer" 
+              aria-label="Visit Instagram profile" 
+              className="contact__text"
+              title='This is a link to JF graphics instagram page'>
+              Go to our Instagram page
             </a>
           </aside>
           <aside className="contact__social-media">
             <img src={youtube} alt="YouTube icon" className="social-media-icon" />
-            <a href="https://www.youtube.com/@johnnyforsman3054" target="_blank" rel="noopener noreferrer" aria-label="Visit YouTube channel" className="contact__text">
+            <a href="https://www.youtube.com/@johnnyforsman3054" 
+              target="_blank" rel="noopener noreferrer" 
+              aria-label="Visit YouTube channel" 
+              className="contact__text" 
+              title='This is a link to JF graphics youtube page'>
               Go to our YouTube page
             </a>
           </aside>
