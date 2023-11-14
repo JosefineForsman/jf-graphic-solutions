2ef6a60bfd6b4bd507071c5dbee4d22bed3682cd
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-14 11:29:21 +0100

    testing new video

--- jf-graphic-solutions/src/components/VideoElement/VideoElement.tsx
+++ jf-graphic-solutions/src/components/VideoElement/VideoElement.tsx
@@ -1,10 +1,15 @@
 import './VideoElement.css';
+import movie from '../../assets/4e4fb55a-cdf3-4b76-a467-167584da1b20.mp4'
 
 function VideoElement(){
 
     return(
         <section className='video-element'>
             <h2>Commercial Video</h2>
+            <video width="640" height="360" controls>
+        <source src={movie} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
         </section>
 
     )
