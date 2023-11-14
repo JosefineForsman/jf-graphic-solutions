d662a854d7d17ff138b9ba0cc93bf000ed9f9187
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-14 11:03:32 +0100

    -

---
+++ jf-graphic-solutions/src/components/VideoElement/VideoElement.tsx
@@ -0,0 +1,12 @@
+import './VideoElement.css';
+
+function VideoElement(){
+
+    return(
+        <section className='video-element'>
+            <h2>Commercial Video</h2>
+        </section>
+
+    )
+}
+export default VideoElement;
--- jf-graphic-solutions/src/views/Home/Home.tsx
+++ jf-graphic-solutions/src/views/Home/Home.tsx
@@ -1,18 +1,17 @@
 import './Home.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import VideoElement from '../../components/VideoElement/VideoElement';
 function Home(){
     return (
         <div>
-          <header>
             <Header/>
-          </header>
-    
           <main>
             <section className='home'>
               <h1 className='text__title'>Welcome to JF Graphic Solutions</h1>
               <h2 className='text'>  Maybe you are considering taking the step into social media with effective ads? JF Graphic Solutions is your trusted partner in photography and video production with customized editing for all your business needs.</h2>
             </section>
+            <VideoElement/>
             <section className='home__services'>
               <h1 className='text__title'>Our services</h1>
               <ul className='home__services__ul'>
