c1de6fabaa82b9ebe6220647a1260b6c560ca8c6
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-22 13:01:59 +0100

    icon in error message

--- jf-graphic-solutions/src/components/Form/Form.tsx
+++ jf-graphic-solutions/src/components/Form/Form.tsx
@@ -3,6 +3,7 @@ import { ZodType, z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect } from 'react';
+import error from '../../assets/error-svgrepo-com.svg'
 
 type FormData = {
     firstName: string,
@@ -40,16 +41,16 @@ const handleOnBeforeUnload = (event: BeforeUnloadEvent) => {
       <form  className='form' onSubmit={handleSubmit(submitData)}>
         <label className='form-label' htmlFor="firstname">First Name:</label>
         <input className='form-input' id ='firstname' type="text" {...register('firstName')}/>
-        {errors.firstName && <span className='errors'>{errors.firstName.message}</span>}
+        {errors.firstName && <span className='errors'><img src={error} alt="error-icon" className='error-icon' />{errors.firstName.message}</span>}
         <label className='form-label' htmlFor="lastname">Last Name:</label>
         <input className='form-input' id='lastname' type="text" {...register('lastName')}/>
-        {errors.lastName && <span className='errors'>{errors.lastName.message}</span>}
+        {errors.lastName && <span className='errors'><img src={error} alt="error-icon" className='error-icon' />{errors.lastName.message}</span>}
         <label className='form-label' htmlFor="email">Email:</label>
         <input className='form-input' id='email' type="email" {...register('email')}/>
-        {errors.email && <span className='errors'>{errors.email.message}</span>}
+        {errors.email && <span className='errors'><img src={error} alt="error-icon" className='error-icon' />{errors.email.message}</span>}
         <label className='form-label' htmlFor="message">Message:</label>
         <input className='form-input-textarea' id='message'  {...register('message')} type='text' />
-        {errors.message && <span className='errors'>{errors.message.message}</span>}
+        {errors.message && <span className='errors'><img src={error} alt="error-icon" className='error-icon' />{errors.message.message}</span>}
         <input className='form-button'type='submit' />
       </form>
   );
