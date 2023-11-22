4333c450e26370f99d42ebc71d80c850d2a332ce
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-22 10:22:10 +0100

    added message when realoading and back tab

--- jf-graphic-solutions/src/components/Form/Form.tsx
+++ jf-graphic-solutions/src/components/Form/Form.tsx
@@ -1,7 +1,8 @@
 import './Form.css'
 import {ZodType, z} from 'zod'
 import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
+import {zodResolver} from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
 
 type FormData = {
     firstName: string,
@@ -23,22 +24,33 @@ const {register, handleSubmit, formState:{errors}} = useForm<FormData>({resolver
 const submitData = (data: FormData) =>{
     console.log('it worked', data)
 }
+const handleOnBeforeUnload = (event: BeforeUnloadEvent) => {
+    event.preventDefault();
+    event.returnValue = '';
+  }
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleOnBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleOnBeforeUnload);
+    };
+  }, []);
 
   return (
       <form  className='form' onSubmit={handleSubmit(submitData)}>
         <label className='form-label' htmlFor="firstname">First Name:</label>
-        <input id ='firstname' type="text" {...register('firstName')}/>
+        <input className='form-input' id ='firstname' type="text" {...register('firstName')}/>
         {errors.firstName && <span className='errors'>{errors.firstName.message}</span>}
         <label className='form-label' htmlFor="lastname">Last Name:</label>
-        <input id='lastname' type="text" {...register('lastName')}/>
+        <input className='form-input' id='lastname' type="text" {...register('lastName')}/>
         {errors.lastName && <span className='errors'>{errors.lastName.message}</span>}
         <label className='form-label' htmlFor="email">Email:</label>
-        <input id='email' type="email" {...register('email')}/>
+        <input className='form-input' id='email' type="email" {...register('email')}/>
         {errors.email && <span className='errors'>{errors.email.message}</span>}
         <label className='form-label' htmlFor="message">Message:</label>
-        <input id='message' type='text' {...register('message')} />
+        <input className='form-input-textarea' id='message'  {...register('message')} type='text' />
         {errors.message && <span className='errors'>{errors.message.message}</span>}
-        <input type='submit' />
+        <input className='form-button'type='submit' />
       </form>
   );
 }
