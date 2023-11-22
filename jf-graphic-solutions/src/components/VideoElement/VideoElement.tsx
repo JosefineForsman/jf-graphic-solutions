3c803e87d1a3d3403d4adccca8dbcc875dd426ca
Josefine Forsman <josefineforsman1@gmail.com>
2023-11-22 15:30:44 +0100

    media queries added again

--- jf-graphic-solutions/src/components/VideoElement/VideoElement.tsx
+++ jf-graphic-solutions/src/components/VideoElement/VideoElement.tsx
@@ -4,13 +4,18 @@ function VideoElement(){
 
     return(
         <section className='video-element'>
-            <p className='video-element__text'>This video features a satisfied customer of Samster AB, sharing their positive experience in choosing solar panels from this reputable company. Discover the journey of a happy customer who opted for sustainable solutions provided by Samster AB</p>
+            <article className='video-container'>
+                <hr  className='home__info-hr'/>
+                <p className='video-element__text'>This video features a satisfied customer of Samster AB, sharing their positive experience in choosing solar panels from this reputable company. Discover the journey of a happy customer who opted for sustainable solutions provided by Samster AB</p>
+                <hr  className='home__info-hr '/>
+            </article>
             <video controls>
                 <source src='/movie.mp4' type="video/mp4" />
                 <track kind="captions" src='/subtitles.vtt' srcLang='sv' label='Swedish' />
                 <track kind="captions" src='/subtitles_en.vtt' srcLang='en' label='English' />
                     Your browser does not support the video tag.
             </video>
+
         </section>
 
     )
